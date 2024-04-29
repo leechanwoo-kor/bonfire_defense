f26aa8f19c90ee78dd75869ad3ddc551e347d766
CHANWOO LEE <cwlee1124@gmail.com>
2024-04-29 16:35:17 +0900

    :fire: Refactor routes.dart and remove stages_page.dart

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,9 +1,8 @@
 import 'package:bonfire_defense/screens/about_page.dart';
-import 'package:flutter/material.dart';
-import 'package:bonfire_defense/screens/stages_page.dart';
-import 'package:bonfire_defense/screens/menu_page.dart';
 import 'package:bonfire_defense/screens/game.dart';
+import 'package:bonfire_defense/screens/menu_page.dart';
 import 'package:bonfire_defense/util/stages.dart';
+import 'package:flutter/material.dart';
 
 class AppRoutes {
   static const String homeRoute = '/';
@@ -15,7 +14,6 @@ class AppRoutes {
     return {
       homeRoute: (_) => const MenuPage(),
       aboutRoute: (_) => const AboutPage(),
-      stagesRoute: (_) => const StagesPage(),
       gameRoute: (context) => BonfireDefense(
             config: GameStages.get(
               ModalRoute.of(context)?.settings.arguments as GameStageEnum,
