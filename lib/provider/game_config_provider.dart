ff5a874ada40c2ae3927fbd0399c6ebc20a9dd74
CHANWOO LEE <cwlee1124@gmail.com>
2024-04-30 15:17:29 +0900

    :fire: Refactor GameConfigProvider to use final modifier for _currentConfig

--- lib/provider/game_config_provider.dart
+++ lib/provider/game_config_provider.dart
@@ -2,14 +2,9 @@ import 'package:bonfire_defense/util/game_config.dart';
 import 'package:flutter/material.dart';
 
 class GameConfigProvider extends ChangeNotifier {
-  GameConfig _currentConfig;
+  final GameConfig _currentConfig;
 
   GameConfigProvider(this._currentConfig);
 
   GameConfig get currentConfig => _currentConfig;
-
-  void updateGameConfig(GameConfig newConfig) {
-    _currentConfig = newConfig;
-    notifyListeners();
-  }
 }
