fa5004b7637ce499b2133718f34073813dcfc840
CHANWOO LEE <cwlee1124@gmail.com>
2024-05-10 08:40:36 +0900

    :fire: Refactor CardManager to remove unused dependencies and simplify code structure

--- lib/game_managers/card_manager.dart
+++
@@ -1,9 +0,0 @@
-import 'package:bonfire_defense/components/defenderInfo.dart';
-import 'package:bonfire_defense/provider/game_state_provider.dart';
-
-class CardManager {
-  List<DefenderInfo> availableCards = DefenderInfo.getInfos();
-
-  void selectCard(
-      DefenderInfo card, DefenderStateProvider defenderStateProvider) {}
-}
