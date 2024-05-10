04452bf994a383016b453a12cc67cf40140a5166
CHANWOO LEE <cwlee1124@gmail.com>
2024-05-10 16:38:59 +0900

    :recycle: Refactor defender merge logic and UI to allow merging multiple units at once

--- lib/components/defender.dart
+++ lib/components/defender.dart
@@ -1,10 +1,7 @@
-import 'dart:math';
-
 import 'package:bonfire/bonfire.dart';
-import 'package:bonfire_defense/utils/defender_info.dart';
 import 'package:bonfire_defense/game_managers/defender_manager.dart';
 import 'package:bonfire_defense/provider/defender_state_provider.dart';
-import 'package:bonfire_defense/provider/game_state_provider.dart';
+import 'package:bonfire_defense/utils/defender_info.dart';
 import 'package:bonfire_defense/utils/game_config.dart';
 import 'package:bonfire_defense/widgets/defender_detail_overlay.dart';
 import 'package:flutter/material.dart';
@@ -63,56 +60,14 @@ abstract class Defender extends SimpleAlly with TapGesture {
             defender: this,
             defenderInfo: defenderInfo,
             onClose: () => Navigator.pop(context),
-            onSell: () => sellDefender(context),
-            onMerge: canMerge ? () => mergeDefender(context) : null,
+            onSell: () => DefenderManager.sellDefender(this),
+            onMerge:
+                canMerge ? () => DefenderManager.mergeDefender(this) : null,
           );
         },
       );
     } catch (e) {
       print('Failed to fetch defender info: $e');
     }
-  }
-
-  void sellDefender(BuildContext context) {
-    int refundAmount = DefenderInfo.getInfo(type).cost ~/ 2;
-    gameRef.context.read<GameStateProvider>().updateGold(refundAmount);
-    removeFromParent();
-    Navigator.pop(context);
-  }
-
-  void mergeDefender(BuildContext context) {
-    List<Defender> sameTypeDefenders = gameRef
-        .query()
-        .whereType<Defender>()
-        .where((d) => d.type == type && d != this)
-        .toList();
-
-    if (sameTypeDefenders.isNotEmpty) {
-      // 머지 유닛 제거
-      final randomDefender =
-          sameTypeDefenders[Random().nextInt(sameTypeDefenders.length)];
-      randomDefender.removeFromParent();
-      removeFromParent();
-
-      gameRef.context
-          .read<DefenderStateProvider>()
-          .decrementDefenderCount(type, 2);
-
-      // 머지 유닛 생성
-      Vector2 mergePosition = position.clone();
-      gameRef.context.read<GameStateProvider>().updateGold(-100);
-      createUpgradedDefender(mergePosition, context);
-    }
-    Navigator.pop(context);
-  }
-
-  void createUpgradedDefender(Vector2 position, BuildContext context) {
-    DefenderInfo upgradedInfo = DefenderInfo.getInfo(DefenderType.test);
-    GameComponent upgradedDefender =
-        DefenderManager.createDefender(upgradedInfo, position);
-    gameRef.add(upgradedDefender);
-    gameRef.context
-        .read<DefenderStateProvider>()
-        .addDefender(upgradedInfo.type);
   }
 }
--- lib/game_managers/defender_manager.dart
+++ lib/game_managers/defender_manager.dart
@@ -1,5 +1,9 @@
+import 'dart:math';
+
 import 'package:bonfire/bonfire.dart';
 import 'package:bonfire_defense/components/archer.dart';
+import 'package:bonfire_defense/components/defender.dart';
+import 'package:bonfire_defense/provider/game_state_provider.dart';
 import 'package:bonfire_defense/utils/defender_info.dart';
 import 'package:bonfire_defense/components/knight.dart';
 import 'package:bonfire_defense/components/lancer.dart';
@@ -10,6 +14,7 @@ import 'package:bonfire_defense/game_managers/game_controller.dart';
 import 'package:bonfire_defense/provider/defender_state_provider.dart';
 import 'package:bonfire_defense/screens/game.dart';
 import 'package:bonfire_defense/utils/game_config.dart';
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class DefenderManager {
@@ -49,5 +54,48 @@ class DefenderManager {
       default:
         throw UnimplementedError('Defender type ${info.type} not supported');
     }
+  }
+
+  static void sellDefender(Defender defender) {
+    int refundAmount = DefenderInfo.getInfo(defender.type).cost ~/ 2;
+    defender.gameRef.context.read<GameStateProvider>().updateGold(refundAmount);
+    defender.removeFromParent();
+    Navigator.pop(defender.gameRef.context);
+  }
+
+  static void mergeDefender(Defender defender) {
+    List<Defender> sameTypeDefenders = defender.gameRef
+        .query()
+        .whereType<Defender>()
+        .where((d) => d.type == defender.type && d != defender)
+        .toList();
+
+    if (sameTypeDefenders.isNotEmpty) {
+      // 머지 유닛 제거
+      final randomDefender =
+          sameTypeDefenders[Random().nextInt(sameTypeDefenders.length)];
+      randomDefender.removeFromParent();
+      defender.removeFromParent();
+
+      defender.gameRef.context
+          .read<DefenderStateProvider>()
+          .decrementDefenderCount(defender.type, 2);
+
+      // 머지 유닛 생성
+      Vector2 mergePosition = defender.position.clone();
+      defender.gameRef.context.read<GameStateProvider>().updateGold(-100);
+      createUpgradedDefender(mergePosition, defender);
+    }
+    Navigator.pop(defender.gameRef.context);
+  }
+
+  static createUpgradedDefender(Vector2 position, Defender defender) {
+    DefenderInfo upgradedInfo = DefenderInfo.getInfo(DefenderType.test);
+    GameComponent upgradedDefender =
+        DefenderManager.createDefender(upgradedInfo, position);
+    defender.gameRef.add(upgradedDefender);
+    defender.gameRef.context
+        .read<DefenderStateProvider>()
+        .addDefender(upgradedInfo.type);
   }
 }
