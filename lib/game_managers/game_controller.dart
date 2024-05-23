f95de50d0acec5158e8fef8065f60290b35a5554
CHANWOO LEE <cwlee1124@gmail.com>
2024-05-23 10:37:42 +0900

    :sparkles: Camera zoom methods in GameController

--- lib/game_managers/game_controller.dart
+++ lib/game_managers/game_controller.dart
@@ -145,4 +145,41 @@ class GameController extends GameComponent {
       }
     });
   }
+
+  // 카메라 기능 구현
+  void zoomIn() {
+    final newZoom = (gameRef.camera.zoom * 1.1).clamp(1.0, 3.0);
+    gameRef.camera.animateZoom(
+      zoom: Vector2(newZoom, newZoom),
+      effectController: EffectController(duration: 0.2),
+    );
+  }
+
+  void zoomOut() {
+    final newZoom = (gameRef.camera.zoom / 1.1).clamp(1.0, 3.0);
+    gameRef.camera.animateZoom(
+      zoom: Vector2(newZoom, newZoom),
+      effectController: EffectController(duration: 0.2),
+    );
+  }
+
+  void moveCameraToPosition(Vector2 position) {
+    gameRef.camera.moveToPositionAnimated(
+      position: position,
+      effectController: EffectController(duration: 1.0),
+    );
+  }
+
+  void followPlayer() {
+    gameRef.camera.moveToPlayerAnimated(
+      effectController: EffectController(duration: 1.0),
+    );
+  }
+
+  void shakeCamera() {
+    gameRef.camera.shake(
+      intensity: 20.0,
+      duration: Duration(seconds: 1),
+    );
+  }
 }
--- lib/screens/game.dart
+++ lib/screens/game.dart
@@ -1,9 +1,9 @@
 import 'package:bonfire/bonfire.dart';
-import 'package:bonfire_defense/utils/sensors/end_game_sensor.dart';
 import 'package:bonfire_defense/components/placeable_area.dart';
 import 'package:bonfire_defense/game_managers/game_controller.dart';
 import 'package:bonfire_defense/provider/game_config_provider.dart';
 import 'package:bonfire_defense/utils/game_config.dart';
+import 'package:bonfire_defense/utils/sensors/end_game_sensor.dart';
 import 'package:bonfire_defense/widgets/game_overlay.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
@@ -11,88 +11,84 @@ import 'package:provider/provider.dart';
 
 class BonfireDefense extends StatefulWidget {
   static const tileSize = 16.0;
-  const BonfireDefense({
-    super.key,
-  });
+
+  const BonfireDefense({super.key});
 
   @override
   State<BonfireDefense> createState() => _BonfireDefenseState();
 }
 
 class _BonfireDefenseState extends State<BonfireDefense> {
-  late GameController controller;
+  late GameController gameController;
   late GameConfig config;
-  late double _currentZoom;
+  late BonfireGame _game;
 
   @override
   void initState() {
     super.initState();
-    controller = GameController();
+    gameController = GameController();
+
     config =
         Provider.of<GameConfigProvider>(context, listen: false).currentConfig;
-    _currentZoom = 1.0;
+
+    _buildGame();
   }
 
-  void _onScaleUpdate(ScaleUpdateDetails details) {
-    setState(() {
-      _currentZoom *= details.scale;
-      _currentZoom = _currentZoom.clamp(0.8, 2.0);
-    });
-  }
+  void _buildGame() {
+    final double mapWidth = config.tilesInWidth * BonfireDefense.tileSize;
+    final double mapHeight = config.tilesInHeight * BonfireDefense.tileSize;
 
-  void _onPointerSignal(PointerSignalEvent event) {
-    if (event is PointerScrollEvent) {
-      setState(() {
-        _currentZoom *= event.scrollDelta.dy > 0 ? 1.1 : 0.9;
-        _currentZoom = _currentZoom.clamp(0.8, 2.0);
-      });
-    }
+    double currentZoom = 1.1;
+
+    _game = BonfireGame(
+      context: context,
+      map: WorldMapByTiled(
+        TiledReader.asset(config.tiledMapPath),
+        objectsBuilder: {
+          'endGame': (properties) {
+            return EndGameSensor(
+              gameController,
+              position: properties.position,
+              size: properties.size,
+            );
+          },
+          'place': (properties) => PlaceableArea(
+                controller: gameController,
+                position: properties.position,
+                size: properties.size,
+              ),
+        },
+      ),
+      cameraConfig: CameraConfig(
+        zoom: currentZoom,
+        initPosition: Vector2(mapWidth / 2, mapHeight),
+      ),
+      components: [gameController],
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    final double mapWidth = config.tilesInWidth * BonfireDefense.tileSize;
-    final double mapHeight = config.tilesInHeight * BonfireDefense.tileSize;
-
     return Listener(
-      onPointerSignal: _onPointerSignal,
-      child: GestureDetector(
-        onScaleUpdate: _onScaleUpdate,
-        child: BonfireWidget(
-          map: WorldMapByTiled(
-            TiledReader.asset(config.tiledMapPath),
-            objectsBuilder: {
-              'endGame': (properties) {
-                return EndGameSensor(
-                  controller,
-                  position: properties.position,
-                  size: properties.size,
-                );
-              },
-              'place': (properties) => PlaceableArea(
-                    controller: controller,
-                    position: properties.position,
-                    size: properties.size,
-                  ),
-            },
-          ),
-          // backgroundColor: const Color(0xff85a643),
-          cameraConfig: CameraConfig(
-            initPosition: Vector2(mapWidth / 2, mapHeight),
-            moveOnlyMapArea: true,
-            zoom: _currentZoom,
-            initialMapZoomFit: InitialMapZoomFitEnum.fitWidth,
-          ),
-          components: [
-            controller,
-          ],
-          overlayBuilderMap: {
-            GameOverlay.overlayName: (context, game) => const GameOverlay(),
-          },
-          initialActiveOverlays: const [
-            GameOverlay.overlayName,
-          ],
-        ),
+      onPointerSignal: (pointerSignal) {
+        if (pointerSignal is PointerScrollEvent) {
+          if (pointerSignal.scrollDelta.dy > 0) {
+            gameController.zoomOut();
+          } else {
+            gameController.zoomIn();
+          }
+        }
+      },
+      child: BonfireWidget(
+        map: _game.map,
+        cameraConfig: _game.camera.config,
+        components: _game.world.children.whereType<GameComponent>().toList(),
+        overlayBuilderMap: {
+          GameOverlay.overlayName: (context, game) => const GameOverlay(),
+        },
+        initialActiveOverlays: const [
+          GameOverlay.overlayName,
+        ],
       ),
     );
   }
